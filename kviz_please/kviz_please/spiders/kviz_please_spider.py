936547735c7f2e5cd01b308f66a9adf63e3df54f
Filipp <germanfilipp@yandex.ru>
2020-12-11 20:57:59 +0300

    create parsing kviz please

---
+++ kviz_please/kviz_please/spiders/kviz_please_spider.py
@@ -0,0 +1,40 @@
+import scrapy
+from ..items import SquizItem
+
+
+def format_row(row):
+    result = ""
+    for i in row[0]:
+        if i.isdigit() or i == '.':
+            result += i
+    return result
+
+
+class KvizPlease(scrapy.Spider):
+    name = "kviz_please"
+    start_urls = [
+        'https://quizplease.ru/rating?QpRaitingSearch%5Bgeneral%5D=1&QpRaitingSearch%5Bleague%5D=1&QpRaitingSearch%5Btext%5D=']
+
+    def parse(self, response):
+
+        for div in response.css('.item'):
+            # получение данных о команде
+            name = div.css('.rating-table-row-td1::text').extract()
+            number_game = div.css('.rating-table-kol-game::text').extract()
+            points = div.css('.rating-table-points::text').extract()
+
+            # форматирование строки очков
+            points = format_row(points)
+
+            # Добавление в объект
+            Item = SquizItem()
+            Item['name'] = name[1]
+            Item['number_game'] = int(number_game[1])
+            Item['points'] = float(points)
+
+            yield Item
+
+            for href in response.css('ul.pagination > li.next > a::attr("href")'):
+                if url := response.urljoin(href.extract()):
+                    yield scrapy.Request(url, callback=self.parse)
+
