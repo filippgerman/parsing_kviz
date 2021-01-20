11ce04c4989fc93877de703c85e8a505c288b7a7
Filipp <filippgerman32@gmail.com>
2021-01-20 21:09:46 +0300

    create kvizium parsing, delete kvizium default parsing

---
+++ kvizium_pars/kvizium_pars/pipelines.py
@@ -0,0 +1,54 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+from sqlalchemy import create_engine, Column, BIGINT, String, Integer, FLOAT
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import Session
+from .items import KviziumItem
+import pymysql
+
+pymysql.install_as_MySQLdb()
+Base = declarative_base()
+
+
+class KviziumDb(Base):
+    __tablename__ = 'Квизиум'
+
+    id = Column(BIGINT, primary_key=True, autoincrement=True)
+    name = Column(String(1000, collation='utf8mb4_unicode_ci'), index=True)
+    number_game = Column(Integer)
+    points = Column(FLOAT)
+
+    def __init__(self, name, number_game, points):
+        self.name = name
+        self.number_game = number_game
+        self.points = points
+
+
+class KviziumParsPipeline(object):
+
+    def __init__(self):
+        basename = 'data_scraped'
+        self.engine = create_engine("mysql://huston:fil@localhost/pars", encoding="utf8")
+        Base.metadata.create_all(self.engine)
+        self.session = Session(bind=self.engine)
+
+    def process_item(self, kviz_item, spider):
+        # Проверка нет ли уже такой команды
+        if self.session.query(KviziumDb).filter(KviziumDb.name == kviz_item['name']).count():
+            self.session.query(KviziumDb).filter(KviziumDb.name == kviz_item['name']). \
+                update({KviziumDb.number_game: kviz_item['number_game'], KviziumDb.points: kviz_item['points']},
+                       synchronize_session=False)
+
+        else:
+            result = KviziumDb(kviz_item['name'], kviz_item['number_game'], kviz_item['points'])
+            self.session.add(result)
+
+    def close_spider(self, spider):
+        self.session.commit()
+        self.session.close()
---
+++ kvizium_pars/kvizium_pars/spiders/kvizium_spider.py
@@ -0,0 +1,37 @@
+import scrapy
+from ..items import KviziumItem
+
+
+def remove_spaces(name):
+    """
+    :param name: str название команды
+    :return: str название команды без пробелов в строке
+    """
+    return ' '.join(name.split())
+
+
+class Kvizium(scrapy.Spider):
+    name = "kvizium"
+    start_urls = ['https://quizium.ru/ajaxrating?search_team=&season=all&city=7&json=1&offset=0']
+    number = 10
+
+    def parse(self, response, **kwargs):
+        if response.json().get('rows'):
+            for row in response.json().get('rows'):
+
+                kviz_item = KviziumItem()
+                try:
+                    kviz_item['name'] = remove_spaces(row.get('team_name'))
+                    kviz_item['number_game'] = int(row.get('all_games'))
+                    kviz_item['points'] = float(row.get('points_alltime'))
+                except (KeyError, ValueError):
+                    kviz_item['number_game'] = 0
+                finally:
+                    yield kviz_item
+                print(
+                    f"Название: {row.get('team_name')} Все мигры:{row.get('all_games')} Очков в игре {row.get('points_alltime')}")
+
+            self.number += 10
+            yield scrapy.Request(
+                f'https://quizium.ru/ajaxrating?search_team=&season=all&city=7&json=1&offset={self.number}',
+                callback=self.parse)
